adaa89a8fbf43bda9f12123f81115ece620afbd8
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-29 20:17:37 -0500

    changed avatar to image on admin table, as well as made a uploader for this class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129212517) do
+ActiveRecord::Schema.define(version: 20170130010940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170129212517) do
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.text     "story"
-    t.string   "avatar"
+    t.string   "image"
     t.string   "filter"
     t.string   "title"
     t.datetime "created_at", null: false
