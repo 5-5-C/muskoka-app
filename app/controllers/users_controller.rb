187e679c9f576546ae24cb732f4ee49f40054c9a
Evan Shabsove <evan.shabsove@gmail.com>
2017-02-14 14:44:13 -0500

    changed radio buttons to drop down menu, make it so if bottom thing selected it will change value to nil. changed grey background on entry show page to white

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,10 @@ class UsersController < ApplicationController
     # TODO: clean this entire section up
     @user = current_user
     @entry = Entry.new(user_params["entry"])
+    if @entry.location == "Some other great Canadian Wildrness"
+      @entry.location = nil
+    end
+    raise 'hit'
     @entry.user_id = current_user.id
     if @user.update_attributes(only_user_params)
       @entry.name = current_user.name
