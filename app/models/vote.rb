eb73419d358cd91a8a7cddbfb3229192812d3364
Evan Shabsove <evan.shabsove@gmail.com>
2017-02-22 16:45:20 -0500

    removed voter ip validation, pushing to website to test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :entry
 
-  validates_uniqueness_of :voter_ip, :message => "already voted"
   validates_presence_of :email, :message => "you must provide your email address"
   validates_uniqueness_of :email, :message => "already voted"
 
